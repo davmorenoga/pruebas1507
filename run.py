1f8ddcf51efe1c1013a43875e81c72481415b450
davmorenoga <134525213+davmorenoga@users.noreply.github.com>
2025-07-16 19:16:10 +0000

    i

--- run.py
+++ run.py
@@ -1,5 +1,20 @@
-import uvicorn
-from app import app
+from fastapi import FastAPI
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from starlette.responses import Response
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+app = FastAPI()
+
+REQUEST_COUNT = Counter("app_requests_total", "Número total de solicitudes a la API")
+
+@app.middleware("http")
+async def count_requests(request, call_next):
+    REQUEST_COUNT.inc()
+    return await call_next(request)
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
+
+@app.get("/")
+def root():
+    return {"message": "Hola desde FastAPI"}
