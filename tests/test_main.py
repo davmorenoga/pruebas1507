bb4a34c17f4a1e58c346abd6cb6002422fa5c697
davmorenoga <134525213+davmorenoga@users.noreply.github.com>
2025-07-16 22:46:25 -0500

    prueba

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,3 +11,21 @@ def test_metrics():
     response = client.get("/metrics")
     assert response.status_code == 200
     assert b"python_gc_objects_collected_total" in response.content
+
+def test_add_sale_success():
+    # Crear cliente
+    client_resp = client.post("/clients", json={"name": "Cliente venta"})
+    client_id = client_resp.json()["client_id"]
+
+    # Crear producto
+    product_resp = client.post("/products", json={"name": "Producto venta", "price": 50.0})
+    product_id = product_resp.json()["product_id"]
+
+    # Registrar venta
+    sale_resp = client.post("/sales", json={
+        "client_id": client_id,
+        "product_id": product_id,
+        "quantity": 2
+    })
+    assert sale_resp.status_code == 200
+    assert sale_resp.json()["message"] == "Sale recorded"
