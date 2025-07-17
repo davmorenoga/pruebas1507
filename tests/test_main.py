ad153a8ea6fcc5b446824780d979738471faedbe
davmorenoga <134525213+davmorenoga@users.noreply.github.com>
2025-07-16 22:33:05 -0500

    m

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,13 @@
-from app.main import app
 from fastapi.testclient import TestClient
+from app.main import app  # asegúrate que main.py esté en app/
 
 client = TestClient(app)
 
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hola desde FastAPI"}
 
 def test_metrics():
     response = client.get("/metrics")
     assert response.status_code == 200
-    assert b"app_requests_total" in response.content
+    assert b"python_gc_objects_collected_total" in response.content
